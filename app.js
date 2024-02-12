f08e985dc4fd50419f251dd75bdb248e6f017d13
Daniele Silva <danielepereira9503@gmail.com>
2024-02-12 16:30:33 +0000

    encapsulamento texto em JS

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+function toggleTexto() {
+  const texto = document.getElementById("texto");
 
+  if (texto.style.display === "none" || texto.style.height === "0px") {
+    texto.style.display = "block";
+    texto.style.height = texto.scrollHeight + "px";
+  } else {
+    texto.style.height = "0px";
+  }
+}
