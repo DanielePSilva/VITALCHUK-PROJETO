0dc43e85e6a2bce7b978edaffe96b15fe041b144
Daniele Silva <danielepereira9503@gmail.com>
2024-02-23 16:34:15 +0000

    setut vite criado

--- Vitalchuk-vite/app.js
+++ Vitalchuk-vite/app.js
@@ -1,11 +1,14 @@
 function toggleTexto() {
   const texto = document.getElementById("texto");
+  const buttonRead = document.getElementById("ButtonRead-btn");
 
   if (texto.style.display === "none" || texto.style.height === "0px") {
     texto.style.display = "block";
     texto.style.height = texto.scrollHeight + "px";
+    buttonRead.textContent = "<< Voltar";
   } else {
     texto.style.height = "0px";
+    buttonRead.textContent = "Leia Mais...";
   }
 }
 
@@ -17,19 +20,9 @@ simpleslider.getSlider({
 });
 
 // slider services
-// document.addEventListener("DOMContentLoaded", function () {
+
 const slider = simpleslider.getSlider({
   container: document.getElementById("trabalhos"),
   transitionTime: 0,
   delay: 3.5,
 });
-
-//   document.getElementById("prevBtn").addEventListener("click", function () {
-//     slider.prevSlide(); // Função para ir para o slide anterior
-//   });
-
-//   // Event listener para o botão próximo
-//   document.getElementById("nextBtn").addEventListener("click", function () {
-//     slider.nextSlide(); // Função para ir para o próximo slide
-//   });
-// });
