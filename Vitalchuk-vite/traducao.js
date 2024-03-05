e2823c0d4b44c8a71743f950ffc530905144ac19
Daniele Silva <danielepereira9503@gmail.com>
2024-03-05 17:02:11 +0000

    build

--- Vitalchuk-vite/traducao.js
+++ Vitalchuk-vite/traducao.js
@@ -1,28 +1,28 @@
-const traducoes = {
-  pt: {
-    titulo: "VITALCHUK Climatização",
-    sobre: "Sobre Nós",
-    serviço: "Serviços",
-    contactos: "Contactos",
+// const traducoes = {
+//   pt: {
+//     titulo: "VITALCHUK Climatização",
+//     sobre: "Sobre Nós",
+//     serviço: "Serviços",
+//     contactos: "Contactos",
 
-    // Adicione outras traduções conforme necessário
-  },
-  en: {
-    titulo: "VITALCHUK Air Conditioning",
-    sobre: "About Us",
-    serviços: "Services",
-    contactos: "Contacts",
+//     // Adicione outras traduções conforme necessário
+//   },
+//   en: {
+//     titulo: "VITALCHUK Air Conditioning",
+//     sobre: "About Us",
+//     serviços: "Services",
+//     contactos: "Contacts",
 
-    // Adicione outras traduções conforme necessário
-  },
-};
+//     // Adicione outras traduções conforme necessário
+//   },
+// };
 
-function alterarIdioma(idioma) {
-  const elementos = document.querySelectorAll("[data-traducao]");
-  elementos.forEach((elemento) => {
-    const chaveTraducao = elemento.getAttribute("data-traducao");
-    if (traducoes[idioma][chaveTraducao]) {
-      elemento.textContent = traducoes[idioma][chaveTraducao];
-    }
-  });
-}
+// function alterarIdioma(idioma) {
+//   const elementos = document.querySelectorAll("[data-traducao]");
+//   elementos.forEach((elemento) => {
+//     const chaveTraducao = elemento.getAttribute("data-traducao");
+//     if (traducoes[idioma][chaveTraducao]) {
+//       elemento.textContent = traducoes[idioma][chaveTraducao];
+//     }
+//   });
+// }
